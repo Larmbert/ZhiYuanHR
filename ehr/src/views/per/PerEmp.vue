f585dcfda17a61601d37649a1ae676c8af429a1c
Larmbert <812540633@qq.com>
2020-07-12 22:22:10 +0800

    1.添加人事管理中员工资料界面 2.添加新增操作员功能

--- ehr/src/views/per/PerEmp.vue
+++ ehr/src/views/per/PerEmp.vue
@@ -1,13 +1,194 @@
 <template>
-    <div>员工资料</div>
+    <div style="margin-top: 10px">
+        <el-table
+                v-loading="loading"
+                element-loading-text="拼命加载中"
+                element-loading-spinner="el-icon-loading"
+                element-loading-background="rgba(0, 0, 0, 0.8)"
+                :data="emps"
+                stripe
+                style="width: 100%">
+<!--                @selection-change="handleSelectionChange">-->
+<!--            <el-table-column-->
+<!--                    type="selection"-->
+<!--                    width="55">-->
+<!--            </el-table-column>-->
+            <el-table-column
+                    prop="name"
+                    fixed
+                    label="姓名"
+                    width="90">
+            </el-table-column>
+            <el-table-column
+                    prop="workID"
+                    label="工号"
+                    width="100">
+            </el-table-column>
+            <el-table-column
+                    prop="gender"
+                    label="性别">
+            </el-table-column>
+            <el-table-column
+                    prop="birthday"
+                    label="出生日期"
+                    width="100">
+            </el-table-column>
+            <el-table-column
+                    prop="idCard"
+                    label="身份证号"
+                    width="150">
+            </el-table-column>
+            <el-table-column
+                    prop="wedlock"
+                    label="婚姻状况">
+            </el-table-column>
+            <el-table-column
+                    prop="nation.name"
+                    label="民族">
+            </el-table-column>
+            <el-table-column
+                    prop="nativePlace"
+                    label="籍贯">
+            </el-table-column>
+            <el-table-column
+                    prop="politicsstatus.name"
+                    label="政治面貌">
+            </el-table-column>
+            <el-table-column
+                    prop="email"
+                    label="电子邮件"
+                    width="150">
+            </el-table-column>
+            <el-table-column
+                    prop="phone"
+                    label="电话号码"
+                    width="100">
+            </el-table-column>
+            <el-table-column
+                    prop="address"
+                    label="联系地址"
+                    width="180">
+            </el-table-column>
+            <el-table-column
+                    prop="tiptopDegree"
+                    label="最高学历">
+            </el-table-column>
+            <el-table-column
+                    prop="school"
+                    label="毕业院校">
+            </el-table-column>
+            <el-table-column
+                    prop="department.name"
+                    label="所属部门"
+                    width=" 160px">
+            </el-table-column>
+            <el-table-column
+                    prop="position.name"
+                    label="职位">
+            </el-table-column>
+            <el-table-column
+                    prop="jobLevel.name"
+                    label="职称"
+                    width="100">
+            </el-table-column>
+            <el-table-column
+                    prop="engageForm"
+                    label="聘用形式">
+            </el-table-column>
+            <el-table-column
+                    prop="beginDate"
+                    label="入职日期"
+                    width="100">
+            </el-table-column>
+            <el-table-column
+                    prop="conversionTime"
+                    label="转正日期"
+                    width="100">
+            </el-table-column>
+            <el-table-column
+                    prop="beginContract"
+                    label="合同起始日期"
+                    width="100">
+            </el-table-column>
+            <el-table-column
+                    prop="endContract"
+                    label="合同截止日期"
+                    width="100">
+            </el-table-column>
+            <el-table-column
+                    label="合同期限">
+                <template slot-scope="scope">
+                    <el-tag>{{scope.row.contractTerm}}</el-tag>
+                    年
+                </template>
+            </el-table-column>
+        </el-table>
+        <div>
+            <el-pagination
+                    background
+                    @current-change="currentChange"
+                    @size-change="sizeChange"
+                    style="display:flex;justify-content: flex-end"
+                    layout="sizes, prev, pager, next, jumper, ->, total, slot"
+                    :total="total">
+            </el-pagination>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "PerEmp"
+        name: "PerEmp",
+        data(){
+            return{
+                emps: [],
+                loading: false,
+                total: 0,
+                page: 1,
+                size: 10
+            }
+        },
+        mounted() {
+            this.initPersonals();
+        },
+        methods:{
+            sizeChange(currentsize) {
+                this.size = currentsize;
+                this.initPersonals();
+            },
+            currentChange(currentpage) {
+                this.page = currentpage;
+                this.initPersonals();
+            },
+            initPersonals(){
+                this.loading=true;
+                this.getRequest("/personnel/emp/?page="+this.page+"&size"+this.size).then(resp=>{
+                    this.loading=false;
+                    if (resp){
+                      this.emps=resp.data;
+                      this.total=resp.total;
+                    };
+                });
+            }
+        }
     }
 </script>
 
-<style scoped>
+<style>
+    /* 可以设置不同的进入和离开动画 */
+    /* 设置持续时间和动画函数 */
+    .slide-fade-enter-active {
+        transition: all .5s ease;
+    }
 
+    .slide-fade-leave-active {
+        transition: all .5s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+    }
+
+    .slide-fade-enter, .slide-fade-leave-to
+        /* .slide-fade-leave-active for below version 2.1.8 */
+    {
+        transform: translateX(10px);
+        opacity: 0;
+    }
 </style>
--- ehr/src/views/sys/SysHr.vue
+++ ehr/src/views/sys/SysHr.vue
@@ -4,10 +4,13 @@
             <el-input v-model="keywords" placeholder="通过用户名搜索用户..." prefix-icon="el-icon-search"
                       style="width: 400px;margin-right: 10px" @keydown.enter.native="doSearch"></el-input>
             <el-button icon="el-icon-search" type="primary" @click="doSearch">搜索</el-button>
+            <el-button type="primary" icon="el-icon-plus" style="justify-content: flex-end"
+                       @click="showAddHrView">添加用户
+            </el-button>
         </div>
         <div class="hr-container">
             <el-card class="hr-card" v-for="(hr,index) in hrs" :key="index">
-                <div slot="header" class="clearfix" >
+                <div slot="header" class="clearfix">
                     <span>{{hr.name}}</span>
                     <el-button style="float: right; padding: 3px 0;color: red"
                                type="text" icon="el-icon-delete" @click="deleteHr(hr)"></el-button>
@@ -21,17 +24,20 @@
                         <div>手机号码：{{hr.phone}}</div>
                         <div>电话号码：{{hr.telephone}}</div>
                         <div>地址：{{hr.address}}</div>
-                        <div>用户状态：<el-switch
-                                v-model="hr.enabled"
-                                @change="updateHr(hr)"
-                                active-text="启用"
-                                active-color="#13ce66"
-                                inactive-color="#ff4949"
-                                inactive-text="禁用">
-                        </el-switch>
+                        <div>用户状态：
+                            <el-switch
+                                    v-model="hr.enabled"
+                                    @change="updateHr(hr)"
+                                    active-text="启用"
+                                    active-color="#13ce66"
+                                    inactive-color="#ff4949"
+                                    inactive-text="禁用">
+                            </el-switch>
                         </div>
-                        <div>用户角色：<el-tag type="success" style="margin-left: 4px"
-                        v-for="(role,indexr) in hr.roles" :key="indexr">{{role.nameZh}}</el-tag>
+                        <div>用户角色：
+                            <el-tag type="success" style="margin-left: 4px"
+                                    v-for="(role,indexr) in hr.roles" :key="indexr">{{role.nameZh}}
+                            </el-tag>
                             <el-popover
                                     placement="right"
                                     title="角色列表"
@@ -47,13 +53,93 @@
                                             :value="r.id">
                                     </el-option>
                                 </el-select>
-                                <el-button slot="reference" type="text" icon="el-icon-more" style="margin-left: 4px"></el-button>
+                                <el-button slot="reference" type="text" icon="el-icon-more"
+                                           style="margin-left: 4px"></el-button>
                             </el-popover>
                         </div>
                         <div>备注：{{hr.remark}}</div>
                     </div>
                 </div>
             </el-card>
+            <el-dialog
+                    title="添加操作员"
+                    :visible.sync="dialogVisible"
+                    width="60%" style="font-size: xx-small">
+                <div>
+                    <el-form :model="addhr"
+                             :rules="rules" ref="perForm">
+                        <el-row>
+                            <el-col :span="8">
+                                <el-form-item label="姓名:" prop="name">
+                                    <el-input size="mini" style="width: 180px" prefix-icon="el-icon-edit"
+                                              v-model="addhr.name" placeholder="请输入姓名"></el-input>
+                                </el-form-item>
+                            </el-col>
+                            <el-col :span="8">
+                                <el-form-item label="账户名:" prop="username">
+                                    <el-input size="mini" style="width: 180px" prefix-icon="el-icon-edit"
+                                              v-model="addhr.username" placeholder="请输入账户名"></el-input>
+                                </el-form-item>
+                            </el-col>
+                            <el-col :span="8">
+                                <el-form-item label="手机号:" prop="phone">
+                                    <el-input size="mini" style="width: 180px" prefix-icon="el-icon-edit"
+                                              v-model="addhr.phone" placeholder="请输入手机号"></el-input>
+                                </el-form-item>
+                            </el-col>
+                            <el-col :span="8">
+                                <el-form-item label="电话:" prop="telephone">
+                                    <el-input size="mini" style="width: 180px" prefix-icon="el-icon-edit"
+                                              v-model="addhr.telephone" placeholder="请输入电话"></el-input>
+                                </el-form-item>
+                            </el-col>
+                            <el-col :span="8">
+                                <el-form-item label="地址:" prop="address">
+                                    <el-input size="mini" style="width: 180px" prefix-icon="el-icon-edit"
+                                              v-model="addhr.address" placeholder="请输入地址"></el-input>
+                                </el-form-item>
+                            </el-col>
+                            <el-col :span="8">
+                                <el-form-item label="是否启用:" prop="enabled">
+                                    <el-switch
+                                            v-model="addhr.enabled"
+                                            active-text="启用"
+                                            active-color="#13ce66"
+                                            inactive-color="#ff4949"
+                                            inactive-text="禁用">
+                                    </el-switch>
+                                </el-form-item>
+                            </el-col>
+                        </el-row>
+                        <el-row>
+                            <el-col :span="8">
+                                <el-form-item label="密码:" prop="password">
+                                    <el-input type="password" size="mini" style="width: 180px" prefix-icon="el-icon-edit"
+                                              v-model="addhr.password" placeholder="请输入密码"></el-input>
+                                </el-form-item>
+                            </el-col>
+                            <el-col :span="16">
+                                <el-form-item label="头像地址:" prop="userface">
+                                    <el-input size="mini" style="width: 300px" prefix-icon="el-icon-edit"
+                                              v-model="addhr.userface" placeholder="请输入图像url地址"></el-input>
+                                </el-form-item>
+                            </el-col>
+                        </el-row>
+                        <el-row>
+                            <el-col :span="16">
+                                <el-form-item label="备注:" prop="remark">
+                                    <el-input size="mini" style="width: 400px" prefix-icon="el-icon-edit"
+                                              v-model="addhr.remark" placeholder="请输入备注"></el-input>
+                                </el-form-item>
+                            </el-col>
+                        </el-row>
+                    </el-form>
+                </div>
+                <span slot="footer" class="dialog-footer">
+    <el-button size="small" @click="dialogVisible = false">取 消</el-button>
+    <el-button size="small" type="primary" @click="addorEditEmployee">确 定</el-button>
+  </span>
+            </el-dialog>
         </div>
     </div>
 </template>
@@ -61,109 +147,172 @@
 <script>
     export default {
         name: "SysHr",
-        data(){
-            return{
-                keywords:'',
-                hrs:[],
-                allRoles:[],
-                selectedRoles:[]
+        data() {
+            return {
+                dialogVisible: false,
+                keywords: '',
+                hrs: [],
+                allRoles: [],
+                selectedRoles: [],
+                addhr: {
+                    name: '',
+                    phone: '',
+                    telephone: '',
+                    address: '',
+                    enabled: true,
+                    username: '',
+                    password: '',
+                    userface: '',
+                    remark: ''
+                },
+                rules: {
+                    name: [{required: true, message: '请输入姓名', trigger: 'blur'}],
+                    phone: [{required: true, message: '请输入手机号', trigger: 'blur'}],
+                    telephone: [{required: true, message: '请输入电话', trigger: 'blur'}],
+                    username: [{required: true, message: '请输入账户名', trigger: 'blur'}],
+                    password: [{required: true, message: '请输入密码', trigger: 'blur'}],
+                    userface: [{required: true, message: '请输入头像', trigger: 'blur'}]
+                },
+                emptyAddHr(){
+                    this.addhr={
+                        name: '',
+                        phone: '',
+                        telephone: '',
+                        address: '',
+                        enabled: true,
+                        username: '',
+                        password: '',
+                        userface: '',
+                        remark: ''
+                    }
+                }
             }
         },
-        mounted() {
+mounted() {
             this.initHrs();
-        },
-        methods:{
-            deleteHr(hr){
-                if(hr.enabled){
-                    this.$message.error("当前用户未停用，不能删除！");
-                }else{
-                    this.$confirm('此操作将永久删除【'+hr.name+'】, 是否继续?', '提示', {
-                        confirmButtonText: '确定',
-                        cancelButtonText: '取消',
-                        type: 'warning'
-                    }).then(() => {
-                       this.deleteRequest("/system/hr/"+hr.id).then(resp=>{
-                           if (resp){
-                               this.initHrs();
-                           }
-                       });
-                    }).catch(() => {
-                        this.$message({
-                            type: 'info',
-                            message: '已取消删除'
-                        });
-                    });
-                }
-            },
-            doSearch(){
-                this.initHrs();
-            },
-            hidepop(hr){
-                let roles = [];
-                Object.assign(roles, hr.roles);
-                let flag = false;
-                if (roles.length != this.selectedRoles.length) {
-                    flag = true;
-                } else {
-                    for (let i = 0; i < roles.length; i++) {
-                        let role = roles[i];
-                        for (let j = 0; j < this.selectedRoles.length; j++) {
-                            let sr = this.selectedRoles[j];
-                            if (role.id == sr) {
-                                roles.splice(i, 1);
-                                i--;
-                                break;
+},
+        methods: {
+            addorEditEmployee(){
+                this.$refs['perForm'].validate(valid=>{
+                    if (valid){
+                        this.postRequest("/system/hr/",this.addhr).then(resp=>{
+                            console.log(this.addhr);
+                            if (resp){
+                                this.dialogVisible=false;
+                                this.initHrs();
+                                this.emptyEmp();
                             }
-                        }
-                    }
-                    if (roles.length != 0) {
-                        flag = true;
-                    }
-                }
-              if (flag){
-                  let url='/system/hr/role?hrid='+hr.id;
-                  this.selectedRoles.forEach(sr=>{
-                      url+='&rids='+sr;
-                  });
-                  this.putRequest(url).then(resp=>{
-                      if (resp){
-                          this.initHrs();
-                      }
-                  });
-              }
-            },
-            showpop(hr){
-              this.initAllRoles();
-              this.selectedRoles=[];
-              let roles=hr.roles;
-              roles.forEach(r=>{
-                  this.selectedRoles.push(r.id);
-              });
-            },
-            initAllRoles(){
-                this.getRequest("/system/hr/roles").then(resp=>{
-                    if (resp){
-                        this.allRoles=resp;
+                        });
                     }
                 });
             },
-            updateHr(hr){
-                delete hr.roles;
-                this.putRequest("/system/hr/",hr).then(resp=>{
-                    if (resp){
-                        this.initHrs();
-                    }
-                })
+            showAddHrView(){
+                    this.dialogVisible = true;
             },
-            initHrs(){
-                this.getRequest("/system/hr/?keywords="+this.keywords).then(resp=>{
-                    if (resp){
-                        this.hrs=resp;
+                deleteHr(hr)
+                {
+                    if (hr.enabled) {
+                        this.$message.error("当前用户未停用，不能删除！");
+                    } else {
+                        this.$confirm('此操作将永久删除【' + hr.name + '】, 是否继续?', '提示', {
+                            confirmButtonText: '确定',
+                            cancelButtonText: '取消',
+                            type: 'warning'
+                        }).then(() => {
+                            this.deleteRequest("/system/hr/" + hr.id).then(resp => {
+                                if (resp) {
+                                    this.initHrs();
+                                }
+                            });
+                        }).catch(() => {
+                            this.$message({
+                                type: 'info',
+                                message: '已取消删除'
+                            });
+                        });
                     }
-                })
+                }
+            ,
+                doSearch()
+                {
+                    this.initHrs();
+                }
+            ,
+                hidepop(hr)
+                {
+                    let roles = [];
+                    Object.assign(roles, hr.roles);
+                    let flag = false;
+                    if (roles.length != this.selectedRoles.length) {
+                        flag = true;
+                    } else {
+                        for (let i = 0; i < roles.length; i++) {
+                            let role = roles[i];
+                            for (let j = 0; j < this.selectedRoles.length; j++) {
+                                let sr = this.selectedRoles[j];
+                                if (role.id == sr) {
+                                    roles.splice(i, 1);
+                                    i--;
+                                    break;
+                                }
+                            }
+                        }
+                        if (roles.length != 0) {
+                            flag = true;
+                        }
+                    }
+                    if (flag) {
+                        let url = '/system/hr/role?hrid=' + hr.id;
+                        this.selectedRoles.forEach(sr => {
+                            url += '&rids=' + sr;
+                        });
+                        this.putRequest(url).then(resp => {
+                            if (resp) {
+                                this.initHrs();
+                            }
+                        });
+                    }
+                }
+            ,
+                showpop(hr)
+                {
+                    this.initAllRoles();
+                    this.selectedRoles = [];
+                    let roles = hr.roles;
+                    roles.forEach(r => {
+                        this.selectedRoles.push(r.id);
+                    });
+                }
+            ,
+                initAllRoles()
+                {
+                    this.getRequest("/system/hr/roles").then(resp => {
+                        if (resp) {
+                            this.allRoles = resp;
+                        }
+                    });
+                }
+            ,
+                updateHr(hr)
+                {
+                    delete hr.roles;
+                    this.putRequest("/system/hr/", hr).then(resp => {
+                        if (resp) {
+                            this.initHrs();
+                        }
+                    })
+                }
+            ,
+                initHrs()
+                {
+                    this.getRequest("/system/hr/?keywords=" + this.keywords).then(resp => {
+                        if (resp) {
+                            this.hrs = resp;
+                        }
+                    })
+                }
             }
         }
-    }
 </script>
 
 <style>
